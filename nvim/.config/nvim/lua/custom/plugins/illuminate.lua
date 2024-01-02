4718e9e502b82e8f20ac33f43eabe79bedc1938d
anto <above@gualaclosures.com>
2024-01-02 14:49:29 +0100

    Deactivate vim-illuminate in certain filetypes

--- nvim/.config/nvim/lua/custom/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/custom/plugins/illuminate.lua
@@ -3,6 +3,11 @@ return {
     lazy = true,
     event = 'BufEnter',
     config = function ()
+      require'illuminate'.configure({
+        filetypes_denylist = {
+          'fugitive', 'help', 'checkhealth'
+        },
+      })
       vim.keymap.set('n', ']]', function ()
         require'illuminate'.goto_next_reference()
       end, { desc = 'goto next reference' })
