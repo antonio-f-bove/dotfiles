a3036a6dece412ce18d8c8eeca4b7334d5105127
anto <above@gualaclosures.com>
2024-01-09 11:21:52 +0000

    Add firenvim

--- nvim/.config/nvim/lua/anto/plugins/init.lua
+++ nvim/.config/nvim/lua/anto/plugins/init.lua
@@ -47,4 +47,35 @@ return {
       { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
     },
   },
+
+  -- {
+  --   'glacambre/firenvim',
+  --   -- Lazy load firenvim
+  --   -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+  --   lazy = not vim.g.started_by_firenvim,
+  --   module = false,
+  --   build = function()
+  --     vim.fn["firenvim#install"](0)
+  --   end
+  -- },
+  {
+    'glacambre/firenvim',
+    cond = not not vim.g.started_by_firenvim,
+    build = function()
+      require("lazy").load({ plugins = {"firenvim"}, wait = true })
+      vim.fn["firenvim#install"](0)
+    end,
+
+    -- configure FireNvim here:
+    config = function()
+      vim.g.firenvim_config = {
+        -- config values, like in my case:
+        localSettings = {
+          [".*"] = {
+            takeover = "never",
+          },
+        },
+      }
+    end
+  },
 }
