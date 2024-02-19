650a797231cb09e4d5c8aff009e9acf760f02acd
anto <above@gualaclosures.com>
2024-02-19 20:48:41 +0100

    restructure

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -123,69 +123,6 @@ require('lazy').setup({
   { 'folke/which-key.nvim', opts = {} },
 
   {
-    'nvim-lualine/lualine.nvim',
-    lazy = false,
-    config = function()
-      local theme = require 'lualine.themes.catppuccin-macchiato'
-      theme.normal.c.bg = nil
-
-      require 'lualine'.setup({
-        options = {
-          icons_enabled = true,
-          theme = 'catppuccin-macchiato',
-          component_separators = '|',
-          section_separators = '',
-          globalstatus = true,
-        },
-        sections = {
-          lualine_x = {
-            {
-              require 'noice'.api.statusline.mode.get_hl,
-              cond = require 'noice'.api.statusline.mode.has,
-              color = { fg = '#ff9e64' }
-            },
-            -- {
-            --   require("noice").api.status.search.get_hl,
-            --   cond = require("noice").api.status.search.has,
-            --   color = { fg = "#ff9e64" },
-            -- },
-            {
-              'searchcount',
-              maxcount = 999,
-              timeout = 500,
-            }
-          }
-        }
-      })
-    end,
-    -- TODO: max line num instead of col num https://github.com/nvim-lualine/lualine.nvim
-  },
-
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    config = function()
-      require 'ibl'.setup({
-        indent = {
-          char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
-        },
-        scope = {
-          enabled = false,
-          show_start = false,
-          show_end = false,
-        }
-      })
-      -- disable indentation on the first level
-      local hooks = require("ibl.hooks")
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
-    end,
-  },
-
-  {
     'numToStr/Comment.nvim',
     opts = {
       toggler = {
@@ -231,66 +168,6 @@ require('lazy').setup({
   },
 
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      presets = {
-        -- command_palette = true,
-        bottom_search = true,  -- use a classic bottom cmdline for search
-        command_palette = false,
-        inc_rename = false,    -- enables an input dialog for inc-rename.nvim
-        long_message_to_split = true,
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-      -- notify = {
-      --   enabled = false,
-      -- },
-      -- -- views = {
-      -- --
-      -- -- },
-      -- routes = {
-      --   view = 'mini',
-      --   filter = { event = 'msg_showmode' },
-      -- },
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
-      },
-      -- messages = {
-      --   enabled = true,            -- enables the Noice messages UI
-      --   -- view_search = false,
-      --   view = "mini",             -- default view for messages
-      --   view_error = "mini",       -- view for errors
-      --   view_warn = "mini",        -- view for warnings
-      --   view_history = "messages", -- view for :messages
-      -- },
-      -- lsp = {
-      --   message = { view = "mini" }
-      -- }
-    },
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    keys = {
-      { '<leader><esc>', '<cmd>Noice dismiss<cr>', { 'n' } },
-      {
-        "<s-enter>",
-        function()
-          require("noice").redirect(vim.fn.getcmdline())
-        end,
-        mode = 'c',
-        { desc = "Redirect Cmdline" }
-      }
-    }
-  },
-
-  {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     opts = {},
@@ -319,7 +196,7 @@ require('lazy').setup({
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
     keys = {
-      { '<leader>ft', '<cmd> TodoTelescope <cr>', '[F]ind [T]odos' },
+      { '<leader>ft', '<cmd> TodoTelescope keywords=TODO,FIX,BUG,HACK,WARN  <cr>', '[F]ind [T]odos' },
     }
   },
 
--- nvim/.config/nvim/lua/anto/plugins/ui.lua
+++ nvim/.config/nvim/lua/anto/plugins/ui.lua
@@ -1,4 +1,65 @@
 return {
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      presets = {
+        -- command_palette = true,
+        bottom_search = true,  -- use a classic bottom cmdline for search
+        command_palette = false,
+        inc_rename = false,    -- enables an input dialog for inc-rename.nvim
+        long_message_to_split = true,
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      -- notify = {
+      --   enabled = false,
+      -- },
+      -- -- views = {
+      -- --
+      -- -- },
+      -- routes = {
+      --   view = 'mini',
+      --   filter = { event = 'msg_showmode' },
+      -- },
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+      -- messages = {
+      --   enabled = true,            -- enables the Noice messages UI
+      --   -- view_search = false,
+      --   view = "mini",             -- default view for messages
+      --   view_error = "mini",       -- view for errors
+      --   view_warn = "mini",        -- view for warnings
+      --   view_history = "messages", -- view for :messages
+      -- },
+      -- lsp = {
+      --   message = { view = "mini" }
+      -- }
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    keys = {
+      { '<leader><esc>', '<cmd>Noice dismiss<cr>', { 'n' } },
+      {
+        "<s-enter>",
+        function()
+          require("noice").redirect(vim.fn.getcmdline())
+        end,
+        mode = 'c',
+        { desc = "Redirect Cmdline" }
+      }
+    }
+  },
+
   {
     "nvim-neo-tree/neo-tree.nvim",
     version = "*",
@@ -59,33 +120,7 @@ return {
   },
 
   {
-    "folke/zen-mode.nvim",
-    lazy = true,
-    keys = {
-      { '<leader>zz', '<cmd> ZenMode <cr>', 'ZenMode' },
-    },
-    opts = {
-      window = {
-        backdrop = 1,
-        options = {
-          -- number = false,
-          -- relativenumber = false,
-          -- signcolumn = "no",
-        },
-      },
-      plugins = {
-        options = {
-          -- ruler = true,
-          -- showcmd = true,
-          laststatus = 0,
-        },
-        tmux = { enabled = true },
-        gitsigns = { enabled = false },
-      },
-    }
-  },
-
-  {
+    -- NOTE: keeping the winbar is not an option for now
     "folke/zen-mode.nvim",
     lazy = true,
     event = 'VeryLazy',
@@ -95,6 +130,7 @@ return {
     opts = {
       window = {
         backdrop = 0,
+        width = 150,
         options = {
           number = true,
           relativenumber = true,
@@ -115,5 +151,68 @@ return {
         -- vim.cmd('UndotreeClose')
       end,
     },
-  }
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    lazy = false,
+    config = function()
+      local theme = require 'lualine.themes.catppuccin-macchiato'
+      theme.normal.c.bg = nil
+
+      require 'lualine'.setup({
+        options = {
+          icons_enabled = true,
+          theme = 'catppuccin-macchiato',
+          component_separators = '|',
+          section_separators = '',
+          globalstatus = true,
+        },
+        sections = {
+          lualine_x = {
+            {
+              require 'noice'.api.statusline.mode.get_hl,
+              cond = require 'noice'.api.statusline.mode.has,
+              color = { fg = '#ff9e64' }
+            },
+            -- {
+            --   require("noice").api.status.search.get_hl,
+            --   cond = require("noice").api.status.search.has,
+            --   color = { fg = "#ff9e64" },
+            -- },
+            {
+              'searchcount',
+              maxcount = 999,
+              timeout = 500,
+            }
+          }
+        }
+      })
+    end,
+    -- TODO: max line num instead of col num https://github.com/nvim-lualine/lualine.nvim
+  },
+
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    config = function()
+      require 'ibl'.setup({
+        indent = {
+          char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+        },
+        scope = {
+          enabled = false,
+          show_start = false,
+          show_end = false,
+        }
+      })
+      -- disable indentation on the first level
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+    end,
+  },
 }
