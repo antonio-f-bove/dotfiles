ebd14bdae1ccf3de57bc5d1f7359af2fee79dc39
anto <above@gualaclosures.com>
2024-02-19 18:33:07 +0100

    leftover from prev commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -230,9 +230,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- TODO: might want to install:
-  -- - marks related (marks.nvim?)
-
   {
     "folke/noice.nvim",
     event = "VeryLazy",
@@ -282,7 +279,6 @@ require('lazy').setup({
     },
     keys = {
       { '<leader><esc>', '<cmd>Noice dismiss<cr>', { 'n' } },
-      -- BUG: not working, maybe because oa alacritty
       {
         "<s-enter>",
         function()
@@ -293,18 +289,6 @@ require('lazy').setup({
       }
     }
   },
-
-  -- {
-  --   "smjonas/inc-rename.nvim",
-  --   opts = {},
-  --   lazy = true,
-  --   keys = {
-  --     -- {'<leader>rn', ':IncRename ', desc = 'IncRename'}
-  --     {'<leader>rn', function ()
-  --       return ':IncRename ' .. vim.fn.expand('<cword>')
-  --     end, desc = 'IncRename'}
-  --   }
-  -- },
 
   {
     'windwp/nvim-autopairs',
@@ -362,31 +346,6 @@ require 'anto.autocommands'
 
 -- [[ Basic Keymaps ]]
 require 'anto.mappings'
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
