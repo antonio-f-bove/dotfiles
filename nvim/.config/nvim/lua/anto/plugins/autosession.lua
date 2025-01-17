34bc7b8e03d19be5900752ed1f3cc4d63b143d00
Antonio Bove <antonio.bove@centricsoftware.com>
2025-01-17 10:13:52 +0100

    Fix autosession config

--- nvim/.config/nvim/lua/anto/plugins/autosession.lua
+++ nvim/.config/nvim/lua/anto/plugins/autosession.lua
@@ -1,9 +1,10 @@
 return {
   'rmagatti/auto-session',
   config = function()
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     require("auto-session").setup {
       log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/projects", "~/Downloads", "/"},
+      suppress_dirs = { "~/", "~/projects", "~/Downloads", "/" },
     }
   end
 }
