a139b28b90b5524a9d0e08b10c0483e2c9a20edf
above <antoniofbove@gmail.com>
2025-01-29 14:20:04 +0100

    Add leap.nvim

---
+++ nvim/.config/nvim/lua/anto/plugins/leap.lua
@@ -0,0 +1,11 @@
+return {
+  'ggandor/leap.nvim',
+  config = function()
+    require 'leap'.create_default_mappings()
+    -- require'leap.user'.set_repeat_keys('<enter>', '<backspace>')
+  end,
+  -- TODO:
+  -- dependencies = {
+  --   'repeat.vim'
+  -- }
+}
