73a1505f5b0401a509cc845895e78466f760ad00
above <antoniofbove@gmail.com>
2026-02-20 14:33:50 +0100

    WIP: notes workflow

--- nvim/.config/nvim/lua/anto/plugins/notes.lua
+++ nvim/.config/nvim/lua/anto/plugins/notes.lua
@@ -1,5 +1,52 @@
 local notes_dir = vim.fn.getenv 'NOTES_HOME'
 
+local function list_zk_commands()
+  local commands = {
+    { cmd = 'ZkNotes', desc = 'Open notes picker' },
+    { cmd = 'ZkNew', desc = 'Create new note', opts = { 'title' } },
+    { cmd = 'ZkMatch', desc = 'Search notes with match' },
+    { cmd = 'ZkEdit', desc = 'Edit notes' },
+    { cmd = 'ZkTags', desc = 'List all tags' },
+    { cmd = 'ZkBacklinks', desc = 'Show backlinks for current note' },
+    { cmd = 'ZkCd', desc = 'Change to notebook directory' },
+    { cmd = 'ZkIndex', desc = 'Index the notebook' },
+    { cmd = 'ZkBuffers', desc = 'Show buffers in notebook' },
+  }
+
+  vim.ui.select(commands, {
+    prompt = 'Zk commands',
+    format_item = function(item)
+      return item.cmd .. ' ' .. item.desc
+    end,
+  }, function(choice)
+    if not choice then
+      return
+    end
+
+    local opts = {}
+    if choice.opts then
+      local function collect_inputs(i)
+        if i > #choice.opts then
+          require('zk.commands').get(choice.cmd)(opts)
+          return
+        end
+        local opt = choice.opts[i]
+        vim.ui.input({
+          prompt = opt .. ': ',
+        }, function(input)
+          if input and input ~= '' then
+            opts[opt] = input
+          end
+          collect_inputs(i + 1)
+        end)
+      end
+      collect_inputs(1)
+    else
+      require('zk.commands').get(choice.cmd) {}
+    end
+  end)
+end
+
 return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
@@ -51,5 +98,12 @@ return {
         },
       }
     end,
+    keys = {
+      {
+        '<leader>zn',
+        list_zk_commands,
+        desc = 'List zk commands',
+      },
+    },
   },
 }
